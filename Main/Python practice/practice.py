85204782bcc21bd8d8ea6ec888aa8ff1160d5b5d
gastonjarju <jarjug@berea.edu>
2022-01-12 21:08:58 -0500

    Added sales taxes.

--- Main/Python practice/practice.py
+++ Main/Python practice/practice.py
@@ -1,36 +1,34 @@
 
+# Function Practice
+# def userName():
+#     name = input("What is your name?")
+#     if name == "Gaston":
+#         print("Keep up the great job")
+#     else:
+#         print("Try next time!")
+#     return name
+#
+# userName()
+
+# 100DAYS OF CODING CHALLENGE
+# DAY 9:
+
 """
-This is a program that calculates a user's total restaurant bill and the tips they intend to give.
-It can also split the bill among the number of people and return the total fo each individual.
+You are going to write a program which will select a random name from a list of names. 
+The person selected will have to pay for everybody's food bill.
+Important: You are not allowed to use the choice() function.
+Line 8 splits the string names_string into individual names and puts them inside a List called names. 
+For this to work, you must enter all the names as name followed by comma then space. e.g. Angela, Ben, Jenny, Michael, Chloe
 """
-print("!!!!!!!!!!!!!!-------------!!!!!!!!!!!!!!!!!!!!")
-# Declaring variables
-tenTip = 1.1
-twelveTip = 1.12
-fifteenTip = 1.15
-# Calculation starts here
-print("Welcome to the Tip Calculator!\n")
-totalBill = (float(input("What was the total bill?\n $")))
-totalBill = "{:.2f}".format(totalBill)
-totalBill = float(totalBill)
-print(totalBill)
-userTip = int(input("How much tip percent would you like to give? 10, 12, 15 \n"))
-numberOfPeople = int(input("How many people to split the bill? \n"))
-if userTip == 10:
-    userTotal = (totalBill /numberOfPeople) * tenTip
-    userTotal = "{:.2f}".format(userTotal)
-    print(f'Each person should pay: {userTotal}')
-elif userTip == 12:
-    userTotal =  (totalBill /numberOfPeople) * twelveTip
-    userTotal = "{:.2f}".format(userTotal)
-    print(f'Each person should pay: ${userTotal}')
-else:
-    userTotal =  (totalBill /numberOfPeople)  * fifteenTip
-    userTotal = "{:.2f}".format(userTotal)
-    print(f'Each person should pay:{userTotal}')
 
+# Split string method
+import random
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+print(names)
+# 🚨 Don't change the code above 👆
 
-
-
-
+random_index = random.randint(0, len(names)-1)
+random_name = names[random_index]
+print(f" {random_name} is going to buy the meal today!")
 
